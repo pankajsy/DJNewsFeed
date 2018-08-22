e7a1b34479c8f1198771557678402c63a8c96970
psyHaCK <psy231@nyu.edu>
2018-08-22 01:04:58 -0400

    sources added

--- core/management/commands/apidata.py
+++ core/management/commands/apidata.py
@@ -3,12 +3,10 @@ import os, csv, json
 from collections import OrderedDict
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'ftsearch.settings'
-from djauth.tasks import *
+from djauth.tasks import apicollectdata
 
 class Command(BaseCommand):
     help = "My test command"
-
     def handle(self, *args, **options):
-        # testcsv()
         # apicollectdata.apply_async((), queue='cronjob')
         apicollectdata()
--- djauth/tasks.py
+++ djauth/tasks.py
@@ -1,8 +1,10 @@
 from __future__ import absolute_import, unicode_literals
-from .celery import app
-import os
-import traceback
 
+from djauth.settings import BASE_NEWS_API, NEWS_API_KEY, SOURCE_API
+from .celery import app
+import os, json, requests, io, boto3
+import traceback
+from core.models import Source
 os.environ['DJANGO_SETTINGS_MODULE'] = 'djauth.settings'
 timeformat = "%Y-%m-%d %H:%M:%S"
 
@@ -10,7 +12,32 @@ timeformat = "%Y-%m-%d %H:%M:%S"
 def add(x, y):
     return x + y
 
-@app.tasks
+# @app.tasks
 def apicollectdata():
+    url = BASE_NEWS_API + SOURCE_API+"?apiKey="+NEWS_API_KEY
+    try:
+        res = requests.get(url).json()
+        if res.get('status')=='ok':
+            sources = res.get('sources')
+            for s in sources:
+                uid= s.get('id') if 'id' in s else ''
+                name= s.get('name') if 'name' in s else ''
+                description= s.get('description') if 'description' in s else ''
+                url= s.get('url') if 'url' in s else ''
+                category= s.get('category') if 'category' in s else ''
+                language= s.get('language') if 'language' in s else ''
+                country= s.get('country') if 'country' in s else ''
 
-    print("done")
+                source, created = Source.objects.update_or_create(uid = uid,
+                                                                  defaults={
+                                                                      "name":name,
+                                                                      "uid":uid,
+                                                                      "desc":description,
+                                                                      "url":url,
+                                                                      "category": category,
+                                                                      "language":language,
+                                                                      "country":country
+                                                                       })
+                print (created)
+    except Exception as e:
+        print (e.message)
